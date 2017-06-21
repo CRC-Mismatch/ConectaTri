26e97ed6eebf2fbe1126f2ff396b86e1cc2e990c
GLM4610 <giulianolauro@gmail.com>
2017-06-20 21:07:12 -0300

    Add header view with select all checkbox

--- app/src/main/java/br/com/wemind/marketplacetribanco/adapters/ProductsSelectAdapter.java
+++ app/src/main/java/br/com/wemind/marketplacetribanco/adapters/ProductsSelectAdapter.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.Filterable;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import br.com.wemind.marketplacetribanco.databinding.ItemProductsSelectHeaderBinding;
 import br.com.wemind.marketplacetribanco.databinding.ItemSimpleProductSelectBinding;
 import br.com.wemind.marketplacetribanco.models.Product;
 
@@ -20,11 +23,15 @@ public class ProductsSelectAdapter
         extends RecyclerView.Adapter<ProductsSelectAdapter.ViewHolder>
         implements Filterable {
 
+    private static final int VIEW_TYPE_DEFAULT = 0;
+    private static final int VIEW_TYPE_HEADER = 1;
     private final Context context;
     private TreeSet<Product> selectedData;
     private ArrayList<Product> data;
     private ArrayList<Product> filteredData;
     private Filter filter = new Filter();
+    private Set<DefaultViewHolder> items = new HashSet<>();
+    private HeaderViewHolder headerHolder;
 
     public ProductsSelectAdapter(Context context, List<Product> data,
                                  Set<Product> selectedData) {
@@ -39,44 +46,50 @@ public class ProductsSelectAdapter
     }
 
     @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return VIEW_TYPE_HEADER;
+        } else {
+            return VIEW_TYPE_DEFAULT;
+        }
+    }
+
+    @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ViewHolder(ItemSimpleProductSelectBinding.inflate(
-                LayoutInflater.from(context), parent, false
-        ));
+        if (viewType == VIEW_TYPE_HEADER) {
+            HeaderViewHolder vh = new HeaderViewHolder(
+                    ItemProductsSelectHeaderBinding.inflate(
+                            LayoutInflater.from(context), parent, false)
+            );
+            headerHolder = vh;
+            return vh;
+
+        } else {
+            DefaultViewHolder vh = new DefaultViewHolder(
+                    ItemSimpleProductSelectBinding.inflate(
+                            LayoutInflater.from(context), parent, false)
+            );
+
+            items.add(vh);
+            return vh;
+        }
+
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
+        if (getItemViewType(position) == VIEW_TYPE_DEFAULT) {
+            ((DefaultViewHolder) holder).setup(data.get(position - 1));
 
-        final Product product = data.get(position);
-
-        holder.b.product.setText(product.getName());
-
-        if (selectedData.contains(product)) {
-            holder.b.checkbox.setChecked(true);
-        } else {
-            holder.b.checkbox.setChecked(false);
+        } else if (getItemViewType(position) == VIEW_TYPE_HEADER) {
+            ((HeaderViewHolder) holder).setup(null);
         }
-        holder.b.getRoot().setOnClickListener(new View.OnClickListener() {
-            CheckBox checkbox = holder.b.checkbox;
-
-            @Override
-            public void onClick(View v) {
-                if (checkbox.isChecked()) {
-                    selectedData.remove(product);
-                    checkbox.setChecked(false);
-
-                } else {
-                    selectedData.add(product);
-                    checkbox.setChecked(true);
-                }
-            }
-        });
     }
 
     @Override
     public int getItemCount() {
-        return filteredData.size();
+        // + 1 so header ViewHolder is counted in
+        return filteredData.size() + 1;
     }
 
     public Filter getFilter() {
@@ -92,13 +105,26 @@ public class ProductsSelectAdapter
         filteredData = new ArrayList<>(data);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ItemSimpleProductSelectBinding b;
-
-        public ViewHolder(ItemSimpleProductSelectBinding binding) {
-            super(binding.getRoot());
-            b = binding;
+    private void setSelectAll(boolean selectAll) {
+        for (DefaultViewHolder vh : items) {
+            vh.b.checkbox.setChecked(selectAll);
         }
+
+        if (selectAll) {
+            selectedData.addAll(data);
+            
+        } else {
+            selectedData.removeAll(data);
+        }
+    }
+
+    public abstract class ViewHolder<T> extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public abstract void setup(T input);
     }
 
     public class Filter extends android.widget.Filter {
@@ -123,6 +149,70 @@ public class ProductsSelectAdapter
         protected void publishResults(CharSequence constraint, FilterResults results) {
             filteredData = (ArrayList<Product>) results.values;
             notifyDataSetChanged();
+        }
+    }
+
+    private class DefaultViewHolder extends ViewHolder<Product> {
+
+        private ItemSimpleProductSelectBinding b;
+
+        public DefaultViewHolder(ItemSimpleProductSelectBinding binding) {
+            super(binding.getRoot());
+            b = binding;
+        }
+
+        @Override
+        public void setup(final Product product) {
+
+            b.product.setText(product.getName());
+
+            if (selectedData.contains(product)) {
+                b.checkbox.setChecked(true);
+            } else {
+                b.checkbox.setChecked(false);
+            }
+            b.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (b.checkbox.isChecked()) {
+                        selectedData.remove(product);
+                        b.checkbox.setChecked(false);
+
+                        headerHolder.setChecked(false);
+
+                    } else {
+                        selectedData.add(product);
+                        b.checkbox.setChecked(true);
+                    }
+                }
+            });
+        }
+    }
+
+    private class HeaderViewHolder extends ViewHolder<Void> {
+        private ItemProductsSelectHeaderBinding b;
+
+        public HeaderViewHolder(ItemProductsSelectHeaderBinding binding) {
+            super(binding.getRoot());
+            b = binding;
+        }
+
+        @Override
+        public void setup(Void input) {
+            b.checkbox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (b.checkbox.isChecked()) {
+                        setSelectAll(true);
+                    } else {
+                        setSelectAll(false);
+                    }
+                }
+            });
+        }
+
+        public void setChecked(boolean checked) {
+            b.checkbox.setChecked(checked);
         }
     }
 }
