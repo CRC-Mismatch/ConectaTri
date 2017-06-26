3898988a911ab12fd36efcde3574a24caf09997d
GLM4610 <giulianolauro@gmail.com>
2017-06-26 16:20:30 -0300

    Add maximum length parameter to FormattingTextWatcher

--- app/src/main/java/br/com/wemind/marketplacetribanco/utils/FormattingTextWatcher.java
+++ app/src/main/java/br/com/wemind/marketplacetribanco/utils/FormattingTextWatcher.java
@@ -7,12 +7,35 @@ import android.text.TextWatcher;
 import br.com.wemind.marketplacetribanco.utils.Formatting.StringFormatter;
 
 public class FormattingTextWatcher implements TextWatcher {
+    private final boolean hasMaxlength;
     private boolean selfChange;
     private StringFormatter formatter;
+    private int maxLength;
 
+    /**
+     * Convenience for FormattingTextWatcher(<tt>formatter</tt>, 0)
+     *
+     * @param formatter
+     */
     public FormattingTextWatcher(StringFormatter formatter) {
+        this(formatter, 0);
+    }
+
+
+    /**
+     * TextWatcher which applies <tt>formatter's</tt> {@link StringFormatter}.format() method
+     * to its input {@link Editable}.
+     *
+     * @param formatter an implementation of {@link StringFormatter}
+     * @param maxLength maximum length of the final output {@link Editable}.
+     *                  If this parameter is less than 1, maximum length is
+     *                  considered to be <em>unlimited</em>.
+     */
+    public FormattingTextWatcher(StringFormatter formatter, int maxLength) {
         selfChange = false;
         this.formatter = formatter;
+        this.maxLength = maxLength;
+        hasMaxlength = maxLength > 0;
     }
 
     @Override
@@ -28,7 +51,12 @@ public class FormattingTextWatcher implements TextWatcher {
         if (selfChange) {
             return;
         }
-
+        if (hasMaxlength && s.length() > maxLength) {
+            // If this Editable exceeds maxLength, trim off exceeding chars
+            selfChange = true;
+            s.delete(maxLength, s.length());
+            selfChange = false;
+        }
         String formatted = formatter.format(s.toString());
         if (formatted != null) {
             selfChange = true;
